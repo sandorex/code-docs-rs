23effc35d8e4464d2746d94b019e6748b9cb4edf
sandorex <rzhw3h@gmail.com>
2024-09-11 23:04:38 +0200

    feat: add struct_docs function, with test

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,55 @@
 #[allow(dead_code)]
 pub trait DocumentedFields {
+    /// Returns all docstrings of the struct itself
+    fn struct_docs() -> Vec<&'static str>;
+
+    /// Returns all struct field types, ex. `Option<String>`
     fn field_types() -> Vec<&'static str>;
+
+    /// Returns all struct field names
     fn field_names() -> Vec<&'static str>;
+
+    /// Returns all docstrings of each field, each line is a separate string
     fn field_docs() -> Vec<Vec<&'static str>>;
 }
 
 #[macro_export]
 macro_rules! code_docs {
     (
-        $(#[$($m:meta)?])*
-        $p:vis struct $name:ident {
+        $(#[$($meta:meta)?])*
+        $vis:vis struct $name:ident {
             $(
-                $(#[$($im:meta)?])*
-                $p2:vis $i:ident : $t:ty ,
+                $(#[$($f_meta:meta)?])*
+                $f_vis:vis $f_ident:ident : $f_type:ty ,
             )*
         }
     ) => {
-            $(#[$($m)?])*
-            $p struct $name {
+            $(#[$($meta)?])*
+            $vis struct $name {
                 $(
-                    $(#[$($im)?])*
-                    $p2 $i : $t ,
+                    $(#[$($f_meta)?])*
+                    $f_vis $f_ident : $f_type ,
                 )*
             }
 
         impl DocumentedFields for $name {
+            fn struct_docs() -> Vec<&'static str> {
+                vec![
+                    $(
+                        $(
+                            stringify!($meta),
+                        )?
+                    )*
+                ].into_iter()
+                 .filter(|y| y.starts_with("doc = r\""))
+                 .map(|y| &y[8..y.len()-1])
+                 .collect::<Vec<_>>()
+            }
+
             fn field_types() -> Vec<&'static str> {
                 vec![
                     $(
-                        stringify!($t),
+                        stringify!($f_type),
                     )*
                 ]
             }
@@ -36,7 +57,7 @@ macro_rules! code_docs {
             fn field_names() -> Vec<&'static str> {
                 vec![
                     $(
-                        stringify!($i),
+                        stringify!($f_ident),
                     )*
                 ]
             }
@@ -46,7 +67,7 @@ macro_rules! code_docs {
                     $(
                         vec![
                             $(
-                                $(stringify!($im),)?
+                                $(stringify!($f_meta),)?
                             )*
                         ],
                     )*
@@ -68,6 +89,8 @@ mod tests {
 
     code_docs! {
         #[derive(PartialEq, Eq, Debug)]
+        /// This is a testing struct
+        /// With two lines of docstring
         struct TestStruct {
             /// This field is u8
             pub field_a: u8,
@@ -77,6 +100,17 @@ mod tests {
             #[allow(unused)]
             field_b: String,
         }
+    }
+
+    #[test]
+    fn test_struct_docs() {
+        let docs = TestStruct::struct_docs();
+        let docs_raw: Vec<&'static str> = vec![
+            " This is a testing struct",
+            " With two lines of docstring",
+        ];
+
+        assert_eq!(docs, docs_raw);
     }
 
     #[test]
