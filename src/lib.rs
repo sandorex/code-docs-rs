94b08b23c623658f5bf94979167fccab11ff7b5c
sandorex <rzhw3h@gmail.com>
2024-10-02 13:37:55 +0200

    feat: add commented_fields function to DocumentedStruct

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,39 @@ pub trait DocumentedStruct {
     /// Returns all docstrings of each field, each line is a separate string
     fn field_docs() -> Vec<Vec<&'static str>>;
 
+    // TODO add nesting so if the type is documented as well then include it
+    /// Returns formatted string showing types of each field and comments above it, the format is
+    /// very similar to raw rust
+    fn commented_fields() -> Result<String, Box<dyn std::error::Error>> {
+        use std::fmt::Write;
+
+        let mut output = String::new();
+        let names = Self::field_names();
+        let docs = Self::field_docs();
+        let types = Self::field_types();
+
+        // just in case so the errors are not so cryptic below
+        assert_eq!(names.len(), docs.len(), "Field names and field docs are not equal");
+        assert_eq!(names.len(), types.len(), "Field names and field types are not equal");
+
+        for (i, field) in names.iter().enumerate() {
+            // remove the extra newline
+            if i != 0 {
+                write!(output, "\n")?;
+            }
+
+            // imitate rust comments
+            for comment in docs.get(i).unwrap() {
+                write!(output, "///{}\n", comment)?;
+            }
+
+            write!(output, "{}: {}\n", field, types.get(i).unwrap())?;
+        }
+
+        Ok(output)
+    }
+}
+
 pub fn filter_docs(meta: &str) -> Option<&str> {
     // i do not want to use regex crate
     meta.strip_prefix("doc = r\"")
@@ -153,5 +186,22 @@ mod tests {
         ];
 
         assert_eq!(docs, docs_raw);
+    }
+
+    #[test]
+    fn test_commented_fields() {
+        let output = TestStruct::commented_fields();
+        assert!(output.is_ok());
+
+        assert_eq!(
+            output.unwrap(),
+            r#"/// This field is u8
+field_a: u8
+
+/// This field is a String
+/// Also two lines of comments
+field_b: String
+"#
+        );
     }
 }
